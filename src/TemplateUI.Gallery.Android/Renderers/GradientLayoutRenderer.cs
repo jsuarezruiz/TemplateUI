00c7beaae2019ad0fb78b54550a699cfc88f8207
Huy Hoang Nguyen <Huy.Hoang.Nguyen@msg.group>
2020-11-07 12:00:27 +0100

    [feature] ColorPicker

--- src/TemplateUI.Gallery.Android/Renderers/GradientLayoutRenderer.cs
+++ src/TemplateUI.Gallery.Android/Renderers/GradientLayoutRenderer.cs
@@ -1,67 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
 using Android.Content;
+using Android.Graphics;
 using TemplateUI.Controls;
 using TemplateUI.Gallery.Droid.Renderers;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
-[assembly: ExportRenderer(typeof(OpacityGradientLayout), typeof(GradientLayoutRenderer))]
+[assembly: ExportRenderer(typeof(GradientLayout), typeof(GradientLayoutRenderer))]
 
 // @author: https://stackoverflow.com/users/9654227/na2axl
 namespace TemplateUI.Gallery.Droid.Renderers
 {
     public class GradientLayoutRenderer : VisualElementRenderer<AbsoluteLayout>
     {
-        private Color SelectedColor { get; set; }
+        List<int> droidColorSpectrumArgb = new List<int>();
+        public GradientLayoutRenderer(Context context) : base(context)
+        {
+            List<Xamarin.Forms.Color> colorSpectrum = new List<Xamarin.Forms.Color>
+            {
+                Xamarin.Forms.Color.FromHsla(0.1d, 1.0d, 0.5d),
+                Xamarin.Forms.Color.FromHsla(0.2d, 1.0d, 0.5d),
+                Xamarin.Forms.Color.FromHsla(0.3d, 1.0d, 0.5d),
+                Xamarin.Forms.Color.FromHsla(0.4d, 1.0d, 0.5d),
+                Xamarin.Forms.Color.FromHsla(0.5d, 1.0d, 0.5d),
+                Xamarin.Forms.Color.FromHsla(0.6d, 1.0d, 0.5d),
+                Xamarin.Forms.Color.FromHsla(0.7d, 1.0d, 0.5d),
+                Xamarin.Forms.Color.FromHsla(0.8d, 1.0d, 0.5d),
+                Xamarin.Forms.Color.FromHsla(0.9d, 1.0d, 0.5d),
+                Xamarin.Forms.Color.FromHsla(1.0d, 1.0d, 0.5d),
+            };
 
-        private GradientColorStackMode Mode { get; set; }
+            List<Android.Graphics.Color> droidColorSpectrum = new List<Android.Graphics.Color>();
+            foreach (Xamarin.Forms.Color col in colorSpectrum)
+            {
+                droidColorSpectrum.Add(col.ToAndroid());
+            }
 
-        public GradientLayoutRenderer(Context ctx) : base(ctx)
-        { }
+            foreach (Android.Graphics.Color droidColor in droidColorSpectrum)
+            {
+                this.droidColorSpectrumArgb.Add(droidColor.ToArgb());
+            }
+        }
 
         protected override void DispatchDraw(global::Android.Graphics.Canvas canvas)
         {
-            Android.Graphics.LinearGradient gradient;
-
-            int[] androidColors = new int[] { Color.White.ToAndroid().ToArgb(), SelectedColor.ToAndroid().ToArgb() };
-
-            switch (Mode)
-            {
-                default:
-                case GradientColorStackMode.ToRight:
-                    gradient = new Android.Graphics.LinearGradient(0, 0, Width, 0, androidColors, null, Android.Graphics.Shader.TileMode.Mirror);
-                    break;
-                case GradientColorStackMode.ToLeft:
-                    gradient = new Android.Graphics.LinearGradient(Width, 0, 0, 0, androidColors, null, Android.Graphics.Shader.TileMode.Mirror);
-                    break;
-                case GradientColorStackMode.ToTop:
-                    gradient = new Android.Graphics.LinearGradient(0, Height, 0, 0, androidColors, null, Android.Graphics.Shader.TileMode.Mirror);
-                    break;
-                case GradientColorStackMode.ToBottom:
-                    gradient = new Android.Graphics.LinearGradient(0, 0, 0, Height, androidColors, null, Android.Graphics.Shader.TileMode.Mirror);
-                    break;
-                case GradientColorStackMode.ToTopLeft:
-                    gradient = new Android.Graphics.LinearGradient(Width, Height, 0, 0, androidColors, null, Android.Graphics.Shader.TileMode.Mirror);
-                    break;
-                case GradientColorStackMode.ToTopRight:
-                    gradient = new Android.Graphics.LinearGradient(0, Height, Width, 0, androidColors, null, Android.Graphics.Shader.TileMode.Mirror);
-                    break;
-                case GradientColorStackMode.ToBottomLeft:
-                    gradient = new Android.Graphics.LinearGradient(Width, 0, 0, Height, androidColors, null, Android.Graphics.Shader.TileMode.Mirror);
-                    break;
-                case GradientColorStackMode.ToBottomRight:
-                    gradient = new Android.Graphics.LinearGradient(0, 0, Width, Height, androidColors, null, Android.Graphics.Shader.TileMode.Mirror);
-                    break;
-            }
+            
+            var gradient = new LinearGradient(0, 0, 0, Height,
+            colors: this.droidColorSpectrumArgb.ToArray(), positions: null, tile: Shader.TileMode.Clamp);
 
             var paint = new Android.Graphics.Paint()
             {
                 Dither = true,
             };
-
             paint.SetShader(gradient);
-            canvas.DrawPaint(paint);
-
+            canvas.DrawRect(0, 0, Width, Height, paint);
             base.DispatchDraw(canvas);
         }
 
@@ -70,20 +64,24 @@ namespace TemplateUI.Gallery.Droid.Renderers
             base.OnElementChanged(e);
 
             if (e.OldElement != null || Element == null)
+            {
                 return;
-
+            }
             try
             {
-                if (e.NewElement is OpacityGradientLayout layout)
-                {
-                    SelectedColor = layout.SelectedColor;
-                    Mode = layout.Mode;
-                }
+                var gradientLayout = e.NewElement as GradientLayout;
             }
             catch (Exception ex)
             {
                 System.Diagnostics.Debug.WriteLine(@"ERROR:", ex.Message);
             }
+        }
+
+        private static int convertDpToPixel(float dp)
+        {
+            var mainDisplayInfo = DeviceDisplay.MainDisplayInfo;
+            var pixels = dp * mainDisplayInfo.Density;
+            return (int)pixels;
         }
     }
 }
